d21074c7c04d5fa3cf2bd4595b46dda09b2ac699
badlychadly <chadmontoya21@yahoo.com>
2019-04-10 16:31:27 -0700

    allow server to talk with frontend, add cors

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch('http://10.0.0.99:3001/api/quotes').then(resp => resp.json()).then(data => {
+      debugger;
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        hello
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+const cors = require('cors')
+const path = require('path');
 
+app.use(express.static('build'));
+app.use(cors())
 app.use('/api/quotes', require('./routes/quote-routes'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+  });
 
 app.listen(PORT, () => {
     console.log(`App is up and running. Listening on port ${PORT}`);
